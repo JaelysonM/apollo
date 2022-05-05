fe94fcdbc7acf0f57d1cb4e8d7d9f0216867d53d
= <jaelysonmartins@gmail.com>
2022-05-05 13:55:19 -0300

    Tela de splash screen implementada

---
+++ lib/models/account.dart
@@ -0,0 +1,20 @@
+class Account {
+  String id;
+  String name;
+  String email;
+
+  double? evaluation;
+  double? lastEvaluation;
+  bool? isActive;
+  bool? isAdmin;
+
+  Account({
+    required this.id,
+    required this.name,
+    required this.email,
+    this.evaluation = 5.0,
+    this.lastEvaluation = 5.0,
+    this.isActive = false,
+    this.isAdmin = false,
+  });
+}
